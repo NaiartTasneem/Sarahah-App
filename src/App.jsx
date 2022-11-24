4ed393d564a10ad7c8ed9dc8e4c92840fd5327b5
yaqoot9 <yaqootwishahi@gmail.com>
2022-11-23 16:15:43 -0800

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,35 @@ import Home from './Component/Home/Home';
 import Login from './Component/Login/Login';
 import Navbar from './Component/Navbar/Navbar';
 import Register from './Component/Register/Register';
-
+import jwtDecode from 'jwt-decode';
+import { useEffect, useState } from 'react';
 function App() {
+  
+  let[loginData,setloginData]=useState(null);
+
+  function setUserData(){
+    let token=localStorage.getItem('token');
+    let decoded=jwtDecode(token);
+    setloginData(decoded);
+    console.log(loginData);
+  }
+
+  useEffect(()=>{  
+  if(localStorage.getItem('token')){
+    setUserData();
+  }
+  
+},[])
+
+
+
   return (
     <>
     <Navbar/>
     <div className="container">
     <Routes>
       <Route path='home' element={<Home/>} ></Route>
-      <Route path='login' element={<Login />} ></Route>
+      <Route path='login' element={<Login  setUserData={setUserData}  />} ></Route>
       <Route path='register' element={<Register/>} ></Route>
     </Routes>
     </div>
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,6 +1,48 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom'; 
 
-function Login() {
+function Login({setUserData}) {
+
+  let navegate=useNavigate();
+  function  goToHome(){
+    let path='/home';
+    navegate(path);
+  }
+
+  let SubmitFormData= async (e)=>{
+
+    e.preventDefault();
+    let{data}=await axios.post("http://localhost:3003/api/v1/auth/signin",user);
+   if (data.message=='login')
+   {localStorage.setItem('token',data.loginToken);
+   setUserData();
+   goToHome();
+
+   }
+  }
+  
+  let [user,setuser]=useState({
+    email:"",
+    password:"",
+  
+  }
+    
+    );
+  
+    let DataForm= (e)=>{
+     let myuser={...user};
+      myuser[e.target.name]=e.target.value;
+      setuser(myuser);
+    }
+  
+  
+  
+
+
+
+
   return (
  <div className="container text-center my-5">
   <div className="user my-3">
@@ -8,10 +50,10 @@ function Login() {
     <h4 className="login">Login</h4>
   </div>
   <div className="card p-5 w-50 m-auto">
-    <form method="POST" action="/handleLogin">
-      <input className="form-control" placeholder="Enter your email" type="text" name="email" />
-      <input className="form-control my-4 " placeholder="Enter your Password" type="text" name="password" />
-      <button className="btn btn-default-outline my-4 w-100 rounded">Login</button>
+    <form   onSubmit={SubmitFormData}  method="POST" action="/handleLogin">
+      <input className="form-control" placeholder="Enter your email" type="text" name="email" onChange={DataForm} />
+      <input className="form-control my-4 " placeholder="Enter your Password" type="text" name="password" onChange={DataForm}/>
+      <button  type="submit" className="btn btn-default-outline my-4 w-100 rounded">Login</button>
       <p><a className="text-muted forgot btn" href>I Forgot My Password</a></p>
       <a className="btn btn-default-outline" href="register.html">Register</a>
     </form>
