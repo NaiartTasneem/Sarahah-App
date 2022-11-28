1eb1668b949b4f6e1e89d7de1c3bc51d07df1511
yaqoot9 <yaqootwishahi@gmail.com>
2022-11-27 23:09:53 -0800

    Baerer Token

--- src/Component/userprofile/UserProfile.jsx
+++ src/Component/userprofile/UserProfile.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 function UserProfile() {
-    
+
+  
+  async function getmessages(){
+    let token=localStorage.getItem('token');
+    let tariq='tariq__';
+    //alert(`Bearer ${tariq}${token}`);
+   let {data}=await axios.get(`http://localhost:3000/api/v1/message/`, { headers: {"Authorization" : `${tariq}${token}`} });
+  
+   console.log(data);
+  }
+
+useEffect(()=>{getmessages()}
+,[])
+
   return (
-    
+    <>
     <h2 className="text-center">Welcome </h2>
+     
+
+
+    </>
   )
 }
 
