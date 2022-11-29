71f887eafb4e2d2c1865e8997eda3de692f4a6f3
yaqoot9 <yaqootwishahi@gmail.com>
2022-11-28 21:36:49 -0800

    get Messages

--- src/Component/userprofile/UserProfile.jsx
+++ src/Component/userprofile/UserProfile.jsx
@@ -1,24 +1,34 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import style from './userprofile.module.css';
 function UserProfile() {
 
   
   async function getmessages(){
     let token=localStorage.getItem('token');
     let tariq='tariq__';
-    //alert(`Bearer ${tariq}${token}`);
    let {data}=await axios.get(`http://localhost:3000/api/v1/message/`, { headers: {"Authorization" : `${tariq}${token}`} });
-  
-   console.log(data);
+  setmessages(data.messageList);
+   console.log(data.messageList);
   }
+  let[messages,setmessages]=useState([]);
 
 useEffect(()=>{getmessages()}
 ,[])
 
   return (
     <>
-    <h2 className="text-center">Welcome </h2>
+    <h2 className="text-center pt-5 pb-4 fs-3 ">WELCOME </h2>
+    <p className={`${style.mes }`}>Your messages </p>
+    {messages.map((mes,index)=>
+      
+    <div className='border d-flex pb-3'>{mes.text}
+    <p className={`${style.delbtn } `}><a href='#' className='btn btn-danger color-light '>Delete</a></p>
+    </div>
+
+
+
+    )}
      
 
 
